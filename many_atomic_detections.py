3cd1585568f1dac91d0c7bb77d347b05d08c0073
Boaz Zimbler <bzimbler@gmail.com>
2023-07-08 20:27:24 +0300

    changing the model load command to fit for falcon model (trust remote code = true)

--- many_atomic_detections.py
+++ many_atomic_detections.py
@@ -125,7 +125,7 @@ def main():
     else:
         model = AutoModelForCausalLM.from_pretrained(lm_name, load_in_8bit=True)
 
-if torch.backends.mps.is_available():
+    if torch.backends.mps.is_available():
         device = 'mps'
     elif torch.cuda.is_available():
         device = 'cuda'
