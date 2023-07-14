69626b252ae9efb5f121cba289039094f86184e6
Boaz Zimbler <bzimbler@gmail.com>
2023-07-14 15:05:17 +0300

    Deleting the 8 bit quntizesing

--- many_atomic_detections.py
+++ many_atomic_detections.py
@@ -121,9 +121,9 @@ def main():
     logging.debug(f"Loading Language model {lm_name}...")
     tokenizer = AutoTokenizer.from_pretrained(lm_name)
     if lm_name == "tiiuae/falcon-40b" or "tiiuae/falcon-7b":
-        model = AutoModelForCausalLM.from_pretrained(lm_name,trust_remote_code=True)
+        model = AutoModelForCausalLM.from_pretrained(lm_name,load_in_8bit=True,trust_remote_code=True)
     else:
-        model = AutoModelForCausalLM.from_pretrained(lm_name)
+        model = AutoModelForCausalLM.from_pretrained(lm_name, load_in_8bit=True,device_map="auto")
 
     if torch.backends.mps.is_available():
         device = 'mps'
