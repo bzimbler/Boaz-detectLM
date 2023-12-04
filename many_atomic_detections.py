440ebb873423de9c68b418d25641e61f370c9a60
Boaz Zimbler <bzimbler@gmail.com>
2023-12-04 15:32:28 +0200

    adding huggingfave token for llama2 weights acces

--- many_atomic_detections.py
+++ many_atomic_detections.py
@@ -120,6 +120,7 @@ def main():
 
     logging.debug(f"Loading Language model {lm_name}...")
     tokenizer = AutoTokenizer.from_pretrained(lm_name,token='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')
+
     if lm_name == "tiiuae/falcon-40b" or "tiiuae/falcon-7b":
         model = AutoModelForCausalLM.from_pretrained(lm_name,load_in_8bit=True,trust_remote_code=True)
     else:
