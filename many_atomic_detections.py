056c282d2a1bdbbeb302531a613d87ebf29a87ef
Boaz Zimbler <bzimbler@gmail.com>
2023-12-26 18:51:04 +0200

    Some adjusments for phi1.5

--- many_atomic_detections.py
+++ many_atomic_detections.py
@@ -119,11 +119,11 @@ def main():
         context_policy = 'no_context'
 
     logging.debug(f"Loading Language model {lm_name}...")
-    tokenizer = AutoTokenizer.from_pretrained(lm_name,token='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')
+    tokenizer = AutoTokenizer.from_pretrained(lm_name,trust_remote_code=True,token='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')
     if lm_name == "tiiuae/falcon-40b" or lm_name == "tiiuae/falcon-7b":
         model = AutoModelForCausalLM.from_pretrained(lm_name,trust_remote_code=True)
     else:
-        model = AutoModelForCausalLM.from_pretrained(lm_name,device_map="auto",token='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')
+        model = AutoModelForCausalLM.from_pretrained(lm_name,device_map="auto", torch_dtype="auto", trust_remote_code=True,token='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')
 
     if torch.backends.mps.is_available():
         device = 'mps'
